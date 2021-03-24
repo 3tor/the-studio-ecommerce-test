3cc876a8892935722a9324988b18ad120ed249a9
3tor <kudetutopia@gmail.com>
2021-03-24 15:47:19 +0100

    Add user registration feature

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, ['required' => true])
+            ->add('firstname', null, ['required' => true])
+            ->add('lastname', TextType::class, ['required' => true])
+            ->add('phone', TelType::class, ['required' => true])
+            ->add('address', TextType::class, ['required' => true])
+            ->add('password', PasswordType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
